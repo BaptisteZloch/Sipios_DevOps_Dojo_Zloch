64bac2fd4ecb8d91df2184144cae28ef83e22151
BaptisteZloch <bzloch@hotmail.fr>
2022-10-25 14:56:42 +0200

    try catch

--- index.ts
+++ index.ts
@@ -2,19 +2,25 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-
-  const user_guess = await extractGuess(_req);
-  const word_to_find = "chien";
-  const score = await similarity(user_guess, word_to_find);
-
-  return responseBuilder(score, user_guess, word_to_find);
+  try {
+    const user_guess = await extractGuess(_req);
+    const word_to_find = "chien";
+    const score = await similarity(user_guess, word_to_find);
+    return responseBuilder(score, user_guess, word_to_find);
+  } catch (e) {
+    return new Response(`Error !\n${e.message}`, { status: 400 });
+  }
 }
 
-const responseBuilder = (similarityScore: Number, userGuess:String, wordToFind:String) => {
-  return new Response(`Similarity score between ${userGuess} and ${wordToFind} : ${similarityScore}`);
+const responseBuilder = (similarityScore: Number, userGuess: String, wordToFind: String) => {
+  if (similarityScore > 0.5) {
+    return new Response(`Similarity score between ${userGuess} and ${wordToFind} : ${similarityScore}, words are close !`);
+  } else {
+    return new Response(`Similarity score between ${userGuess} and ${wordToFind} : ${similarityScore}, words aren't close !`);
+  }
 }
 
-const similarity = async (word1:String, word2:String) => {
+const similarity = async (word1: String, word2: String) => {
 
   const body = {
     sim1: word1,
