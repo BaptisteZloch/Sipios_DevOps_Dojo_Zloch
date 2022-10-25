dcf8e1b879a911f74b7552e5abe8ac29f6652e1f
BaptisteZloch <bzloch@hotmail.fr>
2022-10-25 14:08:58 +0200

    Similarity implemented

--- index.ts
+++ index.ts
@@ -2,8 +2,24 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+  const body = {
+    sim1: 'Chien',
+    sim2: "Chien",
+    lang: "fr",
+    type: "General Word2Vec",
+  };
+  const similarityResponse = await fetch(
+    "http://nlp.polytechnique.fr/similarityscore",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+  const similarityResponseJson = await similarityResponse.json();
+  return new Response(`Similarity score: ${Number(similarityResponseJson.simscore)}`);
 }
-
 serve(handler);
 
