d7fc4aa9dd6638f121e1f64aa21beee5cc9a805b
BaptisteZloch <bzloch@hotmail.fr>
2022-10-25 14:31:17 +0200

    debug flag

--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
   const user_guess = extractGuess(_req);
+  console.log("user_guess : ", user_guess);
   const word_to_find = "chien";
   const score = await similarity(user_guess, word_to_find);
-  
+  console.log("score : ", score);
+
   return new Response(`Similarity score between ${user_guess} and ${word_to_find} : ${score}`);
 }
 
@@ -17,6 +19,7 @@ const similarity = async (word1, word2) => {
     lang: "fr",
     type: "General Word2Vec",
   };
+  console.log("body : ", body);
   const similarityResponse = await fetch(
     "http://nlp.polytechnique.fr/similarityscore",
     {
